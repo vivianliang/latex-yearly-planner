e7b375fa4b1315a687c80931fce1acaf1a79f7f5
Vivian Liang <vliang88@gmail.com>
2024-12-17 15:22:13 -0800

    daily 'more' notes: include three pages per day instead of just one (terrible code, just works)

--- app/compose/daily.go
+++ app/compose/daily.go
@@ -8,7 +8,7 @@ import (
 
 var Daily = DailyStuff("", "")
 var DailyReflect = DailyStuff("Reflect", "Reflect")
-var DailyNotes = DailyStuff("More", "Notes")
+var DailyNotes = DailyStuffNotes("More", "Notes")
 
 func DailyStuff(prefix, leaf string) func(cfg config.Config, tpls []string) (page.Modules, error) {
 	return func(cfg config.Config, tpls []string) (page.Modules, error) {
@@ -40,6 +40,46 @@ func DailyStuff(prefix, leaf string) func(cfg config.Config, tpls []string) (pag
 								"Extra2":       extra2(cfg.ClearTopRightCorner, false, false, week, 0),
 							},
 						})
+					}
+				}
+			}
+		}
+
+		return modules, nil
+	}
+}
+
+func DailyStuffNotes(prefix, leaf string) func(cfg config.Config, tpls []string) (page.Modules, error) {
+	return func(cfg config.Config, tpls []string) (page.Modules, error) {
+		year := cal.NewYear(cfg.WeekStart, cfg.Year)
+		modules := make(page.Modules, 0, 366)
+
+		for _, quarter := range year.Quarters {
+			for _, month := range quarter.Months {
+				for _, week := range month.Weeks {
+					for _, day := range week.Days {
+						if day.Time.IsZero() {
+							continue
+						}
+						for i := 1; i <= 3; i++ { // make three pages of notes per day
+							modules = append(modules, page.Module{
+								Cfg: cfg,
+								Tpl: tpls[0],
+								Body: map[string]interface{}{
+									"Year":         year,
+									"Quarter":      quarter,
+									"Month":        month,
+									"Week":         week,
+									"Day":          day,
+									"Breadcrumb":   day.NotesBreadcrumb(prefix, leaf, i, cfg.ClearTopRightCorner && len(leaf) > 0),
+									"HeadingMOS":   day.HeadingMOS(prefix, leaf),
+									"SideQuarters": year.SideQuarters(day.Quarter()),
+									"SideMonths":   year.SideMonths(day.Month()),
+									"Extra":        day.PrevNext(prefix).WithTopRightCorner(cfg.ClearTopRightCorner),
+									"Extra2":       extra2(cfg.ClearTopRightCorner, false, false, week, 0),
+								},
+							})
+						}
 					}
 				}
 			}
